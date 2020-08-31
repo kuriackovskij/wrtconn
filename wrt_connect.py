f991371dcc2281ed5c9841ccbe2d8804d44878f2
Alex K <alex@kuriackovskij.com>
2020-08-31 17:37:29 +0100

    Added MACs from arp table as well for full picture

--- wrt_connect.py
+++ wrt_connect.py
@@ -12,17 +12,19 @@ sys.stdout.write(GREEN)
 def nameLookup(mac):
     names = {
     '42:bf:71:29:4d:a4': 'X-A41VALMER',
+    'e8:d0:fc:c9:1c:31': 'X-LENOVALME',
     'c6:ed:0e:97:75:25': 'X-ALEXS9',
     '44:8a:5b:ca:73:33': 'X-MYDESKMM',
-    'c0:ee:fb:f5:d8:20': 'X-OnePLUS',
-    'f8:87:f1:c2:d5:a6': 'X-A_iPhone',
-    '3c:f0:11:05:08:1b': 'X-A_LapWrk',
+    'c0:ee:fb:f5:d8:20': 'X-ONEPLUS',
+    'f8:87:f1:c2:d5:a6': 'X-A_IPHONE',
+    '3c:f0:11:05:08:1b': 'X-A_LAPWRK',
     '24:77:03:26:61:38': 'X-PWR_KUHN',
     '5c:ea:1d:0b:9e:8f': 'X-SONY SIT',
     '50:ec:50:1b:fa:13': 'X-FELIX',
     '04:f0:21:64:02:00': 'X-HIK_ATT',
     '00:17:10:90:df:a8': 'X-VM_BLA1',
-    'e8:b1:fc:3a:b8:b7': 'X-TPX_LAP'
+    'e8:b1:fc:3a:b8:b7': 'X-TPX_LAP',
+    '30:45:96:2b:80:f1': 'X-HW_WATCH'
     }
 
     if mac in names:
@@ -30,10 +32,19 @@ def nameLookup(mac):
     else:
         return "NO NAME"
 
-def data_analys(output):
-    print('--- %d clients detected' % (len(output.splitlines())))
-    for line in output.splitlines():
-        mac = line.split(' ')[1]
+def data_analys(output, arpout):
+    arpdict = {}
+    for arp in arpout.splitlines():
+        arpdict[arp] = 'NO RECORDED HOSTNAME'
+    dhcpdict = {}
+    for dhcplease in output.splitlines():
+        dhcpdict[dhcplease.split(' ')[1]] = dhcplease.split(' ')[3]
+
+    fullist = {**arpdict, **dhcpdict}
+    print('--- %d clients detected' % (len(fullist)))
+
+    for key, line in fullist.items():
+        mac = key
         vendor = 'VENDOR NOT FOUND'
         try:
             vendor = MacLookup().lookup(mac)
@@ -44,8 +55,9 @@ def data_analys(output):
         if name != 'NO NAME':
             hostname = name
         else:
-            hostname = '!!!-'+line.split(' ')[3]
+            hostname = '!!!-'+line
         print('CLIENT: %s [%s]' % (hostname, vendor))
+
 
 def connect(username, password, type, ipaddress, port):
     params = {
@@ -58,9 +70,10 @@ def connect(username, password, type, ipaddress, port):
     login = ConnectHandler(**params)
     login.enable()
     output = login.send_command_expect('cat /tmp/dhcp.leases')
+    arpout = login.send_command_expect("arp -a | awk 'NR>1{print $4}'")
     login.disconnect()
 
-    data_analys(output)
+    data_analys(output, arpout)
 
 
 wrt_pass = getpass.getpass('Enter password: ')
